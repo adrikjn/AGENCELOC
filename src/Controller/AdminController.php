7d16205ab90385c5d0ab45e1e5d4a2817d947245
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-06-14 17:08:37 +0200

    push fin 1ere journee

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Datetime;
+use App\Entity\User;
 use App\Entity\Vehicule;
+use App\Form\UserType;
 use App\Form\VehiculeType;
+use App\Repository\UserRepository;
 use App\Repository\VehiculeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -68,5 +71,37 @@ class AdminController extends AbstractController
         return $this->redirectToRoute("admin_vehicule");
     }
 
+    #[Route('/admin/users', name: 'admin_users')]
+    public function adminUser(UserRepository $repo, EntityManagerInterface $manager){
+        $users = $repo->findAll();
+        
+        return $this->render('admin/gestionUsers.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/admin/users/edit/{id}', name: 'admin_users_edit')]
+    #[Route('/admin/users/new', name: 'admin_users_new')]
+    public function formUser(Request $request, EntityManagerInterface $manager, User $user = null){
+
+        if($user == null){
+            $user = new User;
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user->setDateEnregistrement(new \Datetime);
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('success', 'Le user a bien été enregistré');
+            return $this->redirectToRoute('admin_users');
+        }
+
+        return $this->render('admin/addUsers.html.twig', [
+            'form' => $form,
+            'editUser' => $user->getId()!==null
+        ]);
+    }
     
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -41,15 +41,7 @@ class RegistrationFormType extends AbstractType
                     'Homme' => "m",
                     'Femme' => 'f'
                 ]
-            ])
-            // ->add('agreeTerms', CheckboxType::class, [
-            //     'mapped' => false,
-            //     'constraints' => [
-            //         new IsTrue([
-            //             'message' => 'You should agree to our terms.',
-            //         ]),
-            //     ],
-            // ])
+                ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
