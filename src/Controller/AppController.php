ed4b2f508ffba257a0cf20e107a2bcf917c36b5f
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-06-15 12:39:30 +0200

    debut commande

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use Datetime;
+use App\Entity\Commande;
+use App\Form\CommandeType;
 use App\Repository\VehiculeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +20,26 @@ class AppController extends AbstractController
         $vehicules = $repo->findAll();
         return $this->render('app/index.html.twig', [
             'vehicules' => $vehicules
+        ]);
+    }
+
+    #[Route('/commander', name:'command')]
+    public function commanderVehicule(Request $request, EntityManagerInterface $manager){
+        $commande = new Commande;
+
+        $form = $this->createForm(CommandeType::class, $commande);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $commande->setDateEnregistrement(new Datetime);
+            $manager->persist($commande);
+            $manager->flush();
+            $this->addFlash('success', "Votre commande a bien été pris");
+            return $this->redirectToRoute('app_app');
+        }
+
+        return $this->render('app/addCommande.html.twig', [
+            'form' => $form
         ]);
     }
 }
